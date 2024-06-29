ed71b39463683493c7e05a6868f01ecdf90de0a8
Johnson Jiu <nm1056163602@163.com>
2024-06-29 16:11:24 +0800

    环形链表 II 1.快慢指针 2.哈希表

--- src/algorithm/DetectCycle.java
+++ src/algorithm/DetectCycle.java
@@ -1,41 +1,77 @@
 package algorithm;
 
+import org.junit.Test;
+
+/**
+ * 环形链表 II
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：返回索引为 1 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ */
 public class DetectCycle {
     public ListNode detectCycle(ListNode head) {
-        if(head == null || head.next == null){
+        if(head == null){
             return null;
         }
         ListNode head1 = head;
-        ListNode head2 = head.next.next;
-        while (head2 != null){
+        ListNode head2 = head;
+        boolean flag = false;
+        while (head2.next != null && head2.next.next != null){
             head1 = head1.next;
-            if(head2.next == null){
-                break;
-            }
             head2 = head2.next.next;
-            if(head == head2){
+            if(head1 == head2){
+                flag = true;
                 break;
             }
         }
-        if(head1 != head2){
+        if(flag){
+            while(head != head2){
+                head = head.next;
+                head2 = head2.next;
+            }
+            return head2;
+        }else{
             return null;
         }
+    }
 
-        while (head != head2){
-            head = head.next;
-            if(head == head2){
-                return head;
-            }
-            head2 = head2.next;
-        }
-        return head;
+    @Test
+    public void test(){
+/*        ListNode head = new ListNode(-1);
+        head.next = new ListNode(-7);
+        head.next.next = new ListNode(7);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = new ListNode(19);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(-9);
+        head.next.next.next.next.next.next.next = new ListNode(-5);
+        head.next.next.next.next.next.next.next.next = new ListNode(-2);
+        head.next.next.next.next.next.next.next.next.next = new ListNode(-5);
+        head.next.next.next.next.next.next.next.next.next.next = head.next.next.next.next.next.next;*/
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        ListNode res = detectCycle(head);
+        System.out.println(res.val);
     }
 }
 
+/*public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = head;
+        Set<ListNode> visited = new HashSet<ListNode>();
+        while (pos != null) {
+            if (visited.contains(pos)) {
+                return pos;
+            } else {
+                visited.add(pos);
+            }
+            pos = pos.next;
+        }
+        return null;
+    }
+}*/
 
-/**
 
 
 
- }
- return head == head2;
+
