a8949b6a0e48a5f08061af66b8624d176db40543
zhuosen <nm1056163602@163.com>
2024-07-08 12:08:14 +0800

    最长递增子序列

---
+++ src/algorithm/LengthOfLIS.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 最长递增子序列
+ */
+public class LengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+/* 动态规划
+    作者：Krahets
+    链接：https://leetcode.cn/problems/longest-increasing-subsequence/solutions/24173/zui-chang-shang-sheng-zi-xu-lie-dong-tai-gui-hua-2/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
+
+}
